a4927209fa4f257bea2340324e289ba14257ef77
Sarah Raguin <sarahraguin@gmail.com>
2022-05-12 15:48:29 -0400

    Starting work on user routes

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,30 @@
 const express = require('express');
 const app = express();
+
+const path = require("path");
+const cors = require("cors");
+
+require("dotenv").config();
+
+let helmet = require('helmet');
+app.use(helmet());
+
+/* const commentsRoutes = ;
+const postsRoutes = ;
+const likesRoutes = ;
+const authRoutes = ; */
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
+});
+app.use(cors({
+    origin: process.env.HOST
+}));
+
+
 module.exports = app;
