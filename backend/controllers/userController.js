c46d4159f93b3642a7e63667f1d5b80b1782e27b
Sarah Raguin <sarahraguin@gmail.com>
2022-05-20 16:37:35 -0400

    Still working on login middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,6 +4,7 @@ const sqlRequests = require('../models/userModels');
 const bcrypt = require('bcrypt');
 // permet de créer des tokens d'identification et de les vérifier
 const jwt = require('jsonwebtoken');
+const res = require('express/lib/response');
 
 exports.signup = (req, res, next) => {
     let requestUserMail = JSON.stringify(req.body.email);
@@ -29,39 +30,63 @@ exports.signup = (req, res, next) => {
     });
 };
 
-
 exports.login = (req, res, next) => {
-    let requestUserMail = JSON.stringify(req.body.email);
-    let requestUserPassword = JSON.stringify(req.body.password);
-    let params = [
-        requestUserMail
-    ]
-    let databasePassword = dbfile.db.query(sqlRequests.sqlFindUserPassword, params).toString();
-    console.log(databasePassword);
-    let databaseId = dbfile.db.query(sqlRequests.sqlFindUserId, params).toString();
-    console.log(databaseId);
+    let requestUserMail = req.body.email;
+    let requestUserPassword = req.body.password;
+    let params = [ requestUserMail ];
+    
+    dbfile.db.connect(async function(err) {
+        if(err) {
+            console.log("Impossible de se connecter à la base de donnée");
+        };
+        console.log("Connecté à la base de donnée");
 
-    dbfile.db.query(sqlRequests.sqlFindUserMail, params, function (err, result) {
+        dbfile.db.query(sqlRequests.sqlFindUserMail, params, function (err, result, fields) {
+            if (err) {
+                return res.status(401).json({ error : "Utilisateur non existant"});
+            };
+            res.status(201).json({message : "Utilisateur trouvé " + result[0].password})
+        });
+    })
+
+    /*
+    .findOne({requestUserMail: dbfile.db.query(sqlRequests.sqlFindUserMail, params, function (err, result) {
         if(err) {
             return res.status(401).json({ error : "Utilisateur non trouvé"});
-        };
-        
-        bcrypt.compare(requestUserPassword, databasePassword)
-        .then(valid => {
-            if(!valid) {
-                return res.status(401).json({error : "Mot de passe non valide"});
+        }
+    })})
+    .then(user => {
+        let databasePassword = dbfile.db.query(sqlRequests.sqlFindUserPassword, params, function (err, result, fields) {
+            if (err) {
+                return res.status(401).json({ error : "Email non trouvé"});
+            }
+            console.log(result);
+            res.status(200).json({message : fields.password});
+        });
+    
+        let databaseId = dbfile.db.query(sqlRequests.sqlFindUserId, params, function (err, result, fields) {
+            if (err) {
+                return res.status(401).json({ error : "id non trouvé"});
             };
-            res.status(200).json({
-                userId : databaseId,
-                token : jwt.sign(
-                    {userId : databaseId},
-                    `${process.env.TOKEN}`,
-                    {expiresIn: "24h"}
-                )
-            });
-        }).catch(err => { 
-            console.log("boom: " + err);
-            return res.status(401).json({error : "requestUserPassword=" + requestUserPassword + " databasePassword=" + databasePassword});
+                console.log(result);
+                res.status(200).json({message : fields.id});
+        });
+
+        bcrypt.compare(requestUserPassword, user.databasePassword)
+            .then(valid => {
+                if(!valid) {
+                    return res.status(401).json({ error : "Mot de Passe non valide"});
+                }
+                res.status(200).json({
+                    userId: user.databaseId,
+                    token: jwt.sign(
+                        {userId: user.databaseId},
+                        `${process.env.TOKEN}`,
+                        {expiresIn: "24h"}
+                    )
+                });
+            })
         })
-    })
+        .catch(error => res.status(500).json({error}));
+        */
 };
