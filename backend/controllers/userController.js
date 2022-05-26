f7c5312b9200cb83ce66770580e7717e5cc96471
Sarah Raguin <sarahraguin@gmail.com>
2022-05-26 16:01:33 -0400

    Reworking on controller login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 // permet de créer des tokens d'identification et de les vérifier
 const jwt = require('jsonwebtoken');
 const res = require('express/lib/response');
+const { db } = require('../../../../Projet6/HotTakes_raguin_sarah/backend/Raguin_Sarah_1_CodeApi_032022/models/sauce');
 
 exports.signup = (req, res, next) => {
     let requestUserMail = JSON.stringify(req.body.email);
@@ -37,31 +38,34 @@ exports.login = (req, res, next) => {
     let params = [requestUserMail];
 
     dbfile.db.connect(function (err) {
-        if (err) {
-            console.log("Impossible de se connecter à la base de donnée");
-        };
-        console.log("Connecté à la base de donnée");
+        if(err) {
+            console.log("Impossible de se connecter à la base de données");
+        } else {
+            console.log("Connecté à la base de données");
 
-        dbfile.db.query(sqlRequests.sqlFindUserMail, params, function (err, result, fields) {
-            if (err) {
-                return res.status(401).json({ error: "Utilisateur non existant " });
-            };
-            res.status(201).json({ message: "Utilisateur trouvé " });
-            let valid = bcrypt.compare(requestUserPassword, result[0].password)
-                .then(valid => {
-                    if (valid) {
-                        res.status(200).json({
-                            userId: result[0].id,
-                            token: jwt.sign(
-                                { userId: result[0].id },
-                                `${process.env.TOKEN}`,
-                                { expiresIn: "24h" }
-                            )
-                        });
-                    }
-                    return res.status(401).json({ error: "Mot de Passe non valide" });
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-    })
-};
+            dbfile.db.query(sqlRequests.sqlFindUserMail, params, async function(err, result) {
+                if(err) {
+                    return res.status(401).json({ error : "Utilisateur non existant"});
+                } else {
+                    await bcrypt.compare(requestUserPassword, result[0].password)
+                    .then(valid => {
+                        if(!valid) {
+                            res.status(401).json({ message: "Utilisateur trouvé mais mot de passe non valide" });
+                        } else {
+                            res.status(200).json({
+                                userId: result[0].id,
+                                token: jwt.sign(
+                                    { userId: result[0].id },
+                                    `${process.env.TOKEN}`,
+                                    { expiresIn: "24h" }
+                                )
+                            })
+                            res.status(200).json({ message: "Utilisateur trouvé et mot de passe valide" });
+                        }
+                    })
+                    .catch(error => res.status(500).json({ error }));
+                }
+            })
+        }
+    });
+}
