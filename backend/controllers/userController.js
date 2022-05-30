fb0c30520d42613c8c27aaaa1c220ee0337a4e57
Sarah Raguin <sarahraguin@gmail.com>
2022-05-30 14:59:33 -0400

    Finalizing work on login controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,22 +1,22 @@
 const dbfile = require('../config/db');
 const sqlRequests = require('../models/userModels');
 
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 // permet de créer des tokens d'identification et de les vérifier
 const jwt = require('jsonwebtoken');
 const res = require('express/lib/response');
 const { db } = require('../../../../Projet6/HotTakes_raguin_sarah/backend/Raguin_Sarah_1_CodeApi_032022/models/sauce');
 
 exports.signup = (req, res, next) => {
-    let requestUserMail = JSON.stringify(req.body.email);
-    let requestUserPassword = JSON.stringify(req.body.password);
+    let requestUserMail = req.body.email;
+    let requestUserPassword = req.body.password;
     let hashedPassword = bcrypt.hash(requestUserPassword, 10);
 
     dbfile.db.connect(async function (err) {
         if (err) {
-            console.log("Impossible de se connecter à la base de donnée");
+            console.log("Impossible de se connecter à la base de données");
         };
-        console.log("Connecté à la base de donnée");
+        console.log("Connecté à la base de données");
 
         let params = [
             requestUserMail,
@@ -36,6 +36,7 @@ exports.login = (req, res, next) => {
     let requestUserMail = req.body.email;
     let requestUserPassword = req.body.password;
     let params = [requestUserMail];
+
 
     dbfile.db.connect(function (err) {
         if(err) {
@@ -58,12 +59,11 @@ exports.login = (req, res, next) => {
                                     { userId: result[0].id },
                                     `${process.env.TOKEN}`,
                                     { expiresIn: "24h" }
-                                )
+                                    ),
+                                message: "Utilisateur trouvé et mot de passe valide"
                             })
-                            res.status(200).json({ message: "Utilisateur trouvé et mot de passe valide" });
                         }
                     })
-                    .catch(error => res.status(500).json({ error }));
                 }
             })
         }
