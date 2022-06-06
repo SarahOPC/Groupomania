39e61129001fa7fd1422ebdb2d3c6cb40998557d
Sarah Raguin <sarahraguin@gmail.com>
2022-06-06 15:44:32 -0400

    Finalizing auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,28 +4,21 @@ const jwt = require("jsonwebtoken");
 require('dotenv').config();
 
 module.exports = async (req, res, next) => {
+        
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const decodedToken = jwt.verify(await token, `${process.env.ACCESS_TOKEN_SECRET}`, (err, user) => {
-            if (err) {
-                return res.status(401).json({ error: err.message });
-            }
-            req.user = user;
-            next();
-        });
-        // Vérification du token avec la clé secrète
-        // Récupération du userId dans l'objet Js récupéré
+        const decodedToken = jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`); 
         const userId = decodedToken.userId;
-        // Attribution du userId à l'objet requête
+        // on attribut le userId à l'objet requête
         req.auth = { userId: userId };
-        // si userId dans la requête, comparaison avec celui récupéré du token
-        if (req.body.user_id && req.body.user_id !== userId) {
+        // si j'ai un userId dans la requête, je le compare avec celui récupéré du token
+        if (req.body.userId && req.body.userId !== userId) { // si j'ai un userId dans la requête et qu'il est différent de celui du token
             throw "403: unauthorized request";
         } else {
-            next();
+            next(); // sinon, on peut executer les autres middlewares en jeu
         }
 
     } catch (error) {
-        res.status(401).json({ error: "Requête non authentifiée : " + error.message });
+        res.status(401).json({ error: "Requête non authentifiée: " + error.message });
     }
 };
