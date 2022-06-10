91b450ef33a38176a8d7e116a5e6fb2da60efd18
Sarah Raguin <sarahraguin@gmail.com>
2022-06-10 16:00:41 -0400

    Starting work on likes and dislikes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -8,7 +8,7 @@ exports.createPost = (req, res, next) => {
     const postBodyImage = req.body.image;
 
     // variable = condition ? si oui : sinon
-    let postImage = postBodyImage != '' 
+    let postImage = postBodyImage != ''
         ? `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
         : null;
 
@@ -21,14 +21,14 @@ exports.createPost = (req, res, next) => {
         let params = [
             postBodyText, postImage, postBodyUserId
         ];
-        
+
         dbfile.db.query(sqlRequests.sqlCreatePost, params, function (err, result) {
             if (err) {
-                return res.status(401).json({ message: "Impossible de créer ce post :( "  + err});
+                return res.status(401).json({ message: "Impossible de créer ce post :( " + err });
             };
             res.status(200).json({ message: "Post créée" })
         })
-    
+
     })
 }
 
@@ -77,20 +77,20 @@ exports.deletePost = (req, res, next) => {
         let imageToDelete;
         dbfile.db.query(sqlRequests.sqlGetOneForDelete, params, async function (err, result) {
             if (err) {
-                return res.status(401).json({ message: "Impossible de trouver le post à supprimer " + err});
+                return res.status(401).json({ message: "Impossible de trouver le post à supprimer " + err });
             };
-            let imageToDelete = JSON.stringify(result[0].image); 
-            return imageToDelete;            
+            let imageToDelete = JSON.stringify(result[0].image);
+            return imageToDelete;
         });
 
         if (imageToDelete) {
-                const filename = imageToDelete.split('/images/')[1];
-                fs.unlink(`images/${filename}`, (err) => {
-                    if (err) throw err;
-                    console.log(`images/${filename} was deleted`);
-                })
+            const filename = imageToDelete.split('/images/')[1];
+            fs.unlink(`images/${filename}`, (err) => {
+                if (err) throw err;
+                console.log(`images/${filename} was deleted`);
+            })
         }
-            
+
         dbfile.db.query(sqlRequests.sqlDeletePost, params, function (err, result) {
             if (err) {
                 return res.status(401).json({ message: "Impossible de supprimer ce post :( " + err });
@@ -140,11 +140,70 @@ exports.getOnePost = (req, res, next) => {
 };
 
 exports.ratingPost = (req, res, next) => {
-    /*
-        Chercher le contenu de la requête
-        Vérifier que le post existe (id)
-        Mettre à jour le post dans la base de données avec les likes, dislikes, unlikes, undislikes
-        L'afficher
-    */
+    let likes = req.body.likesdislikes; // 1, 0 ou -1
+    let userId = req.body.user_id;
+    let postId = req.params.id;
+    console.log("like de la req = " + req.body.like);
+    console.log("userId de la req = " + req.body.userId);
+    console.log("id de la sauce = " + req.params.id);
 
-};
+    dbfile.db.connect(async function (err) {
+        if (err) {
+            console.log("Impossible de se connecter à la base de données");
+        };
+        console.log("Connecté à la base de données");
+
+        let parameters = [
+            postId, userId, likes
+        ];
+
+        switch (likes) {
+            case 1:
+                dbfile.db.query(sqlRequests.sqlCreatingLike, parameters, function (err, results) {
+                    if (err) {
+                        return res.status(401).json({ message: "Impossible de liker le post :(" });
+                    };
+                    res.status(200).json({ results });
+                })
+                break;
+            case -1:
+                dbfile.db.query(sqlRequests.sqlCreatingLike, parameters, function (err, results) {
+                    if (err) {
+                        return res.status(401).json({ message: "Impossible de disliker le post :(" });
+                    };
+                    res.status(200).json({ results });
+                })
+                break;
+            case 0:
+                dbfile.db.query(sqlRequests.sqlLikesDislikes, parameters, function (err, results) {
+                    if (err) {
+                        return res.status(401).json({ message: "Impossible d'afficher le post demandé' :(" });
+                    };
+                    let resultLikes = JSON.stringify(results[0].value);
+                    return resultLikes;
+                })
+                // si ma valeur = 1 dans BDD
+                let parameter = [
+                        likes
+                ];
+
+                if (resultLikes = 1) {
+                    dbfile.db.query(sqlRequests.sqlUpdateLike, parameter, function (err, resultat) {
+                        if (err) {
+                            return res.status(401).json({ message: "Impossible d'unliker ce post :(" });
+                        };
+                        res.status(200).json({ resultat });
+                    })
+
+                } else if (resultLikes = -1) { // si ma valeur = -1 dans BDD
+                    dbfile.db.query(sqlRequests.sqlUpdateLike, parameter, function (err, resultat) {
+                        if (err) {
+                            return res.status(401).json({ message: "Impossible d'undisliker ce post :(" });
+                        };
+                        res.status(200).json({ resultat });
+                    })
+                }
+                break;
+        }
+    });
+}
--- backend/models/postModels.js
+++ backend/models/postModels.js
@@ -5,7 +5,10 @@ const sqlRequests = {
     sqlDeletePost : "DELETE FROM posts WHERE id = ?",
     sqlUpdatePost : "UPDATE posts SET text = ?, image = ?, user_id = ? WHERE id = ?" ,
     sqlCreatePost : "INSERT INTO posts (text, image, user_id) VALUES (?, ?, ?)",
-    sqlGetOneForDelete : "SELECT image FROM posts WHERE id = ?"
+    sqlGetOneForDelete : "SELECT image FROM posts WHERE id = ?",
+    sqlLikesDislikes : "SELECT * FROM likesdislikes WHERE post_id = ? AND user_id = ?",
+    sqlCreatingLike : "INSERT INTO likesdislikes (post_id, user_id, values) VALUES (?, ?, ?)",
+    sqlUpdateLike : "UPDATE likesdislikes SET value = ?"
 };
 
 module.exports = sqlRequests;
