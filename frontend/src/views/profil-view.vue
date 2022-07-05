c5e36f20a8f557bd18263e3cbb70f8b2b80afc47
Sarah Raguin <sarahraguin@gmail.com>
2022-07-05 16:00:48 -0400

    Route done on profil to update password

--- frontend/src/views/profil-view.vue
+++ frontend/src/views/profil-view.vue
@@ -18,8 +18,8 @@
                     <label for="newPassword">Nouveau mot de passe</label><br>
                     <input type="text" id="newPassword" name="newPassword" placeholder="Nouveau mot de passe"><br>
                     <label for="password">Confirmation du nouveau mot de passe</label><br>
-                    <input type="password" id="password" name="password" placeholder="Nouveau mot de passe"><br>
-                    <InputSubmit content="Valider" />
+                    <input type="password" v-model="password" id="password" name="password" placeholder="Nouveau mot de passe"><br>
+                    <InputSubmit v-on:click="updatePassword()" content="Valider" />
                 </div>
                 <div class="changeview">
                 <InputSubmit content="Supprimer mon compte" onclick="alert('Êtes-vous sûr de vouloir supprimer votre compte ? Attention, cette action est irréversible')" />
@@ -43,6 +43,7 @@ import InputSubmit from '@/components/InputSubmit.vue'
 import TextInput from '@/components/TextInput.vue'
 import SelectButton from '@/components/SelectButton.vue'
 import DefaultAvatar from '@/components/DefaultAvatar.vue'
+import axios from 'axios'
 
 export default {
     name: 'profil-view',
@@ -52,6 +53,30 @@ export default {
     TextInput,
     SelectButton,
     DefaultAvatar
+    },
+    data() {
+        return {
+            password: ''
+        }
+    },
+    methods: {
+        updatePassword() {
+            let validToken = localStorage.getItem('userToken');
+            let userValidToken = validToken.replace(/['"]+/g, '');
+            let id = localStorage.getItem('userId');
+            let urlDesti = process.env.VUE_APP_BACKEND_URL + "/" + id + "/profil/password";
+            
+            axios({method:'put', url: urlDesti, data: {
+                    password: this.password
+                }, headers:{'Authorization': 'Bearer ' + userValidToken},
+            })
+            .then(function(response) {
+                console.log(response);
+            })
+            .catch(function(error) {
+                console.log(error);
+            })
+        }
     }
 }
 </script>
