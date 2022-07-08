516b000319ddea329f6e486424efbefef993377b
Sarah Raguin <sarahraguin@gmail.com>
2022-07-08 11:30:19 -0400

    Working on problems with multer

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -12,8 +12,8 @@ router.put('/:id/profil/firstName', auth, userControllers.updateProfilFirstName)
 router.put('/:id/profil/lastName', auth, userControllers.updateProfilLastName);
 router.put('/:id/profil/service', auth, userControllers.updateProfilService);
 router.put('/:id/profil/avatar', auth, multer, userControllers.updateProfilAvatar);
+
 router.put('/:id/profil/password', auth, userControllers.updateProfilPassword);
 router.delete('/:id/profil', auth, userControllers.deleteProfil);
-
 
 module.exports = router;
