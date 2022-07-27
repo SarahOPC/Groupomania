178e6d2d2c8f853645a39d4993b10fd5002932d2
Sarah Raguin <sarahraguin@gmail.com>
2022-07-27 16:18:44 -0400

    Working on modal

--- frontend/src/views/main-news-feed-view.vue
+++ frontend/src/views/main-news-feed-view.vue
@@ -4,7 +4,7 @@
   :style="{ color: '#FFD7D7', 'margin-right': '1.5em' }" /></router-link>
   <router-link to="/logout"><font-awesome-icon data-bs-toggle="tooltip" title="Me déconnecter" icon="fa-solid fa-right-from-bracket" 
   size="lg" :style="{ color: '#FFD7D7', 'margin-right': '1.5em' }"  /></router-link>
-  <font-awesome-icon data-bs-toggle="tooltip" title="Rôle du médiateur" icon="fa-solid fa-user-secret" size="lg"
+  <font-awesome-icon data-toggle="modal" data-target="#mediatorModal" data-bs-toggle="tooltip" title="Rôle du médiateur" icon="fa-solid fa-user-secret" size="lg"
    :style="{ color: '#FFD7D7', 'margin-right': '1.5em' }" />
 </nav>
   <router-view></router-view>
@@ -16,6 +16,29 @@
       <div class="previousPost">
           <PostComponent />
       </div><br>
+
+    <div class="modal fade" id="modalCenter" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalLongTitle">Rôle du médiateur</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Le médiateur de ce site est présent afin que les échanges restent courtois et sympathiques en tout temps.</p>
+        <p>Cet espace vous ait réservé afin de vous détendre et de pouvoir communiquer plus facilement avec vos collègues</p>
+        <p>Le médiateur pourra supprimer au besoin et selon sa seule opinion tous post ou commentaire contraires à nos valeurs d'entreprise</p>
+        <p>Merci d'avance de votre collaboration et bon partage</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
     </div>
 </template>
 
