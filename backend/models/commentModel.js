5001e0fe444f09cac605fde1f37a338e871bcf71
Sarah Raguin <sarahraguin@gmail.com>
2022-08-02 16:01:46 -0400

    Adding a route on commentModel for admin purpose

--- backend/models/commentModel.js
+++ backend/models/commentModel.js
@@ -2,8 +2,8 @@
 const sqlRequests = {
     sqlCreateComment: 'INSERT INTO comments (`text`, `userId`, `postId`) VALUES (?, ?, ?)',
     sqlDeleteComment: 'DELETE FROM comments WHERE id = ?',
-    sqlGetAllComments: 'SELECT * FROM comments WHERE postId = ?'
-
+    sqlGetAllComments: 'SELECT * FROM comments WHERE postId = ?',
+    sqlGetRole: "SELECT role FROM users WHERE id = ?"
 };
 
 module.exports = sqlRequests;
